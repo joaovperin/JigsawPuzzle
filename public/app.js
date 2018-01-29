d475316058b3b7a71f6b1e2eb53c1d4f05519a6c
Perin <joaovperin@live.com>
2018-01-29 20:06:23 -0200

    From Australia, first commit.

---
+++ public/app.js
@@ -0,0 +1,20 @@
+/**
+ * Aplicativo
+ */
+function App() {
+    var self = this;
+
+    self.onLoad = function(){
+        console.log('carregou a pagina');
+    };
+
+    self.loadImage = function () {
+        img = document.getElementById('img');
+
+        alert('BURRO');
+
+    };
+    //app.start();
+
+    return self;
+}
